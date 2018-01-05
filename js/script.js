183488837bd93d964b88778e43492b7d5b1116d0
Rajnishj <Rajnishj@github.com>
2018-01-05 16:31:34 +0530

    Html form

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $('#H1BThisYes').on('click', function(){
+//     	$('#details').toggleClass('hide');
+//     })
+
+
+
+
+
+$('#H1BThisYes').on('click', function(e){
+    	e.preventDefault()
+    	$('#details').toggleClass('hide');
+    })
